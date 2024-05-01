553f9675d893e39e97c871863865cd21b6b9c9c3
Nagham <Naghamkheir25@gmail.com>
2024-05-01 06:07:35 -0400

    part 3
    This Repo is part of an ESP32 based project. This repo contains part 3 of the firmware.

---
+++ ESP32ReceiveA1ValueThenSendinJSformat/ESP32ReceiveA1ValueThenSendinJSformat.ino
@@ -0,0 +1,169 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char *ssid = "ADT";
+const char *password = "adt@12345";
+const char *server_address = "192.168.1.2";
+const int serverPort = 5000;
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, 18, 19);
+
+  // Connect to WiFi
+  connectToWiFi();
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  handleWiFiClientRequests();
+  handleArduinoData();
+  handleSerialData();
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void handleWiFiClientRequests() {
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New WiFi client");
+
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print("Received from client: ");
+        Serial.println(line);
+
+        // Forward data to Arduino via Serial2
+        Serial2.print(line);
+
+        // You can add additional logic here if needed
+      }
+    }
+
+    client.stop();
+    Serial.println("WiFi client disconnected");
+  }
+}
+
+void handleArduinoData() {
+  if (Serial2.available() > 0) {
+    String receivedData = Serial2.readStringUntil('\n');
+    Serial.println("Received from Arduino: " + receivedData);
+
+    // Add logic to handle Arduino data if needed
+  }
+}
+
+void handleSerialData() {
+  if (Serial2.available() > 0) {
+    String receivedData = Serial2.readStringUntil('\n');
+    Serial.println(receivedData);
+
+    if (receivedData.startsWith("Analog Value")) {
+      int sensorValue = receivedData.substring(20).toInt();
+      Serial.println("Sensor Value: " + String(sensorValue));
+
+      // Send the sensor value to the HTTP server
+      sendSensorValueToHTTPServer(sensorValue);
+
+      // Send the sensor value over WiFi
+      sendSensorValueOverWiFi(sensorValue);
+
+      // Fetch emergency level over WiFi
+      int emergencyLevel = fetchEmergencyLevelOverWiFi();
+      Serial.println("Received Emergency Level: " + String(emergencyLevel));
+      Serial2.println("Received Emergency Level: " + String(emergencyLevel));
+      delay(1000);
+    }
+  }
+}
+
+void sendSensorValueToHTTPServer(int sensorValue) {
+  HTTPClient http;
+  String url = "/update";
+  String postData = "value=" + String(sensorValue);
+
+  http.begin(server_address, serverPort, url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpCode = http.POST(postData);
+
+  if (httpCode == 200) {
+    Serial.println("HTTP POST request successful");
+    Serial.println(http.getString());
+  } else {
+    Serial.println("HTTP POST request failed");
+    Serial.println("HTTP Code: " + String(httpCode));
+  }
+
+  http.end();
+  delay(1000);
+}
+
+void sendSensorValueOverWiFi(int value) {
+  WiFiClient client;
+  String url = "/update?value=" + String(value);
+
+  if (client.connect(server_address, serverPort)) {
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + server_address + "\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    delay(1000);
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+    }
+
+    client.stop();
+    Serial.println("Server disconnected");
+  } else {
+    Serial.println("Connection to server failed");
+  }
+}
+
+int fetchEmergencyLevelOverWiFi() {
+  WiFiClient client;
+  String url = "/emergency";
+
+  if (client.connect(server_address, serverPort)) {
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + server_address + "\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    delay(1000);
+
+    while (client.available()) {
+      String line = client.readStringUntil('\n');
+      Serial.print("read this: ");
+      Serial.println(line);
+      if (line.startsWith("{\"level\":")) {
+        int emergencyLevel = line.substring(9).toInt(); // Adjust the substring index
+        Serial.println("we are turning emergency level");
+        Serial.write(emergencyLevel);
+        return emergencyLevel;
+      }
+    }
+
+    client.stop();
+    Serial.println("Server disconnected");
+  } else {
+    Serial.println("Connection to server failed");
+  }
+
+  return 0; // Return an appropriate default value if the fetch fails
+}
---
+++ readingPinA1SendToESP32/readingPinA1SendToESP32.ino
@@ -0,0 +1,99 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial Serial2(0, 1); // Pinout of Arduino to ESP32 for communication with ESP32
+
+const int analogPin = A1;
+const int lamp1Pin = 2;
+const int lamp2Pin = 3;
+const int lamp3Pin = 4;
+const int lamp4Pin = 5;
+const int lamp5Pin = 6;
+
+unsigned long previousMillis = 0;
+const long interval = 100; // Adjust the interval based on your needs
+
+void setup() {
+  Serial.begin(9600); // Arduino serial
+  Serial2.begin(115200);
+  pinMode(lamp1Pin, OUTPUT);
+  pinMode(lamp2Pin, OUTPUT);
+  pinMode(lamp3Pin, OUTPUT);
+  pinMode(lamp4Pin, OUTPUT);
+  pinMode(lamp5Pin, OUTPUT);
+}
+
+void blinkLamp(int lampPin, int blinkDelay) {
+  digitalWrite(lampPin, HIGH);
+  delay(blinkDelay / 2); // On for half of the blink delay
+  digitalWrite(lampPin, LOW);
+  delay(blinkDelay / 2); // Off for the other half of the blink delay
+}
+
+void loop() {
+  if (Serial2.available() > 0) {
+    String receivedData = Serial2.readStringUntil('\n');
+    Serial.println("Received from ESP32: " + receivedData);
+  }
+
+  unsigned long currentMillis = millis();
+
+  // Read the analog value from pin A1
+  int sensorValue = analogRead(analogPin);
+
+  // SENSOR VALUE ON ESP32 SERIAL 115200
+  Serial.print("Analog Value on A1: ");
+  Serial.println(sensorValue);
+
+  // SENSOR VALUE ON ARDUINO SERIAL 9600
+  Serial2.print("SensorValue");
+  Serial2.println(sensorValue);
+
+  delay(900); // Add a delay if needed
+
+  if (Serial.available() > 0) {
+    String emergencyLevel = Serial.readStringUntil('\n');
+    Serial.println(emergencyLevel);
+
+    if (emergencyLevel.startsWith("Received")) {
+      int level = emergencyLevel.substring(26).toInt();
+
+      switch (level) {
+        case 1:
+          // Blink lamp1 continuously
+          if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            blinkLamp(lamp1Pin, 750); // Blink every 750 milliseconds
+          }
+          break;
+        case 2:
+          // Blink lamp2 continuously with faster speed
+          if (currentMillis - previousMillis >= interval / 2) {
+            previousMillis = currentMillis;
+            blinkLamp(lamp2Pin, 600); // Blink every 600 milliseconds
+          }
+          break;
+        case 3:
+          // Blink lamp3 continuously with even faster speed
+          if (currentMillis - previousMillis >= interval / 4) {
+            previousMillis = currentMillis;
+            blinkLamp(lamp3Pin, 450); // Blink every 450 milliseconds
+          }
+          break;
+        case 4:
+          // Blink lamp4 continuously with even faster speed
+          if (currentMillis - previousMillis >= interval / 8) {
+            previousMillis = currentMillis;
+            blinkLamp(lamp4Pin, 300); // Blink every 300 milliseconds
+          }
+          break;
+        case 5:
+          // Blink lamp5 continuously with even faster speed
+          if (currentMillis - previousMillis >= interval / 16) {
+            previousMillis = currentMillis;
+            blinkLamp(lamp5Pin, 150); // Blink every 150 milliseconds
+          }
+          break;
+      }
+    }
+  }
+}
